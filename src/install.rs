797149717a6d1e066154c9cfbd870a5725665535
lennard <44114474+Dummerle@users.noreply.github.com>
2022-09-22 00:19:19 +0200

    Working download package and extract

--- src/install.rs
+++ src/install.rs
@@ -1,13 +1,17 @@
 use std::fs::File;
 use std::io::{Write};
-use std::path::Path;
+use std::path::{Path};
 use std::string::String;
 use std::{fs, io, thread};
+use dirs::data_local_dir;
+// use subprocess::{Popen, PopenConfig, Redirection};
 
-use druid::{Data, Lens, Selector, Target};
-use zip::result::ZipResult;
+use druid::{Data, ExtEventSink, Lens, Selector, Target};
 
 pub(crate) const STATE_UPDATE: Selector<String> = Selector::new("state_update");
+pub(crate) const FINISHED: Selector<()> = Selector::new("finished");
+pub(crate) const ERROR: Selector<String> = Selector::new("error");
+
 
 #[derive(Clone, Copy, PartialEq, Data)]
 pub(crate) enum Version {
@@ -15,11 +19,14 @@ pub(crate) enum Version {
     Git,
 }
 
+pub struct InstallOptions {
+    version: Version,
+}
+
 #[derive(Clone, Data, Lens)]
 pub struct AppState {
-    install_pypresence: bool,
-    install_webview: bool,
     version: Version,
+    pub installing: bool,
     pub(crate) info_text: String,
 }
 
@@ -28,43 +35,81 @@ impl AppState {
         self.info_text = text;
     }
 
+    pub fn get_install_options(&self) -> InstallOptions {
+        InstallOptions {
+            version: self.version,
+        }
+    }
+
     pub fn default() -> AppState {
         return AppState {
-            install_pypresence: false,
-            install_webview: false,
-            version: Version::Stable,
             info_text: "".to_string(),
+            version: Version::Stable,
+            installing: false,
         };
     }
 }
 
-pub fn install(event_sink: druid::ExtEventSink, options: &mut AppState) {
+pub fn install(event_sink: ExtEventSink, _options: InstallOptions) {
     thread::spawn(move || {
         event_sink
-            .submit_command(STATE_UPDATE, "Downloading Rare package".to_string(), Target::Auto)
+            .submit_command(STATE_UPDATE, "Downloading Python".to_string(), Target::Auto)
             .expect("Can't send command");
+
         let filename = match download_file(
-            "https://github.com/Dummerle/Rare/releases/download/1.9.0-rc.3/Rare-Windows-1.9.0.11.zip".to_string(),
+            "https://github.com/Dummerle/Rare/releases/download/1.9.0/Rare-Windows-1.9.0.zip".to_string(),
             // TODO: use env vars
             ".".to_string(),
         ) {
             Ok(filename) => filename,
             Err(err) => {
                 event_sink
-                    .submit_command(STATE_UPDATE, format!("Installation failed: {err}").to_string(), Target::Auto)
+                    .submit_command(ERROR, format!("Installation failed: {err}").to_string(), Target::Auto)
                     .expect("Can't send command");
                 return;
             }
         };
 
-
-        event_sink.submit_command(STATE_UPDATE, "Extracting package".to_string(), Target::Auto)
-            .expect("Can't send command");
-
         match extract_zip_file(filename) {
             Ok(_) => {}
-            Err(_) => {}
+            Err(err) => {
+                event_sink
+                    .submit_command(ERROR, format!("Installation failed: {err}").to_string(), Target::Auto)
+                    .expect("Can't send command");
+                return;
+            }
         }
+
+
+
+        /*
+                event_sink.submit_command(STATE_UPDATE, "Installing python".to_string(), Target::Auto)
+                    .expect("Can't send command");
+
+                let py_install_dir = match install_python(filename.as_str()) {
+                    Ok(path) => path,
+                    Err(err) => {
+                        event_sink.submit_command(ERROR, format!("Failed to install Python: {}", err).to_string(), Target::Auto)
+                            .expect("Can't send command");
+                        return;
+                    }
+                };
+                event_sink.submit_command(STATE_UPDATE, "Installing packages".to_string(), Target::Auto)
+                    .expect("Can't send command");
+
+                match install_packages(py_install_dir, _options.install_pypresence == true,
+                                       _options.install_webview == true, "stable".to_string()) {
+                    Ok(_) => {}
+                    Err(err) => {
+                        event_sink.submit_command(ERROR, format!("Failed to install packages: {}", err).to_string(), Target::Auto)
+                            .expect("Can't send command");
+                        return;
+                    }
+                };
+        */
+        // create desktop shortcut etc
+
+        event_sink.submit_command(FINISHED, (), Target::Auto).expect("Failed to send command")
     });
 }
 
@@ -73,6 +118,13 @@ fn extract_zip_file(filename: String) -> Result<(), String> {
         Ok(file) => { file }
         Err(err) => return Err(err.to_string())
     };
+    let base_path = data_local_dir().unwrap().join("Rare\\Python");
+    if !base_path.exists(){
+        match fs::create_dir_all(&base_path){
+            Ok(_) => {}
+            Err(err) => return Err(err.to_string())
+        };
+    }
 
     let mut archive = match zip::ZipArchive::new(file) {
         Ok(archive) => archive,
@@ -81,10 +133,14 @@ fn extract_zip_file(filename: String) -> Result<(), String> {
 
     for i in 0..archive.len() {
         let mut file = archive.by_index(i).unwrap();
-        let outpath = match file.enclosed_name() {
+
+        let mut outpath = base_path.clone();
+
+        let file_name = match file.enclosed_name() {
             Some(path) => path.to_owned(),
             None => continue,
         };
+        outpath.push(file_name);
 
         {
             let comment = file.comment();
@@ -108,7 +164,7 @@ fn extract_zip_file(filename: String) -> Result<(), String> {
                     fs::create_dir_all(&p).unwrap();
                 }
             }
-            let mut outfile = fs::File::create(&outpath).unwrap();
+            let mut outfile = File::create(&outpath).unwrap();
             io::copy(&mut file, &mut outfile).unwrap();
         }
     }
@@ -144,6 +200,119 @@ fn download_file(url: String, mut dest_path: String) -> Result<String, String> {
         Err(err) => return Err(err.to_string())
     };
 
-    return Ok(file_name);
+    return Ok(dest_path);
+}
+/*
+fn install_python(py_exe_file: &str) -> Result<String, String> {
+    let path = match data_local_dir() {
+        None => return Err("Can't find data dir".to_string()),
+        Some(path) => path
+    };
+    let mut target_dir = PathBuf::new();
+
+    target_dir.push(path);
+    target_dir.push("Rare");
+
+    target_dir.push("python");
+    match fs::create_dir_all(&target_dir) {
+        Ok(_) => {}
+        Err(err) => return Err(err.to_string())
+    };
+
+    // there is probably a better option than chaining methods
+    let mut target_str = "TargetDir=".to_string();
+    target_str.push_str(&target_dir.to_str().unwrap());
+
+    let command = [
+        py_exe_file, "/quiet", py_exe_file, &target_str, "Shortcuts=0",
+        "Include_doc=0", "Include_launcher=0", "Include_tcltk=0", "Include_test=0"
+    ];
+
+    let mut process = match Popen::create(
+        &command, PopenConfig {
+            stdout: Redirection::Pipe,
+            stderr: Redirection::Pipe,
+            ..Default::default()
+        },
+    ) {
+        Ok(process) => process,
+        Err(err) => return Err(err.to_string())
+    };
+
+    // Maybe use out to show debug console
+    let (out, proc_err) = match process.communicate(None) {
+        Ok(res) => res,
+        Err(err) => return Err(err.to_string())
+    };
+
+    if out.is_some() {
+        println!("{}", out.unwrap());
+    }
+
+    if let Some(exit_code) = process.poll() {
+        if exit_code != subprocess::ExitStatus::Exited(0) {
+            return Err(proc_err.unwrap());
+        }
+    }
+    let (out, _) = match process.communicate(None) {
+        Ok(res) => res,
+        Err(err) => return Err(err.to_string())
+    };
+
+    if out.is_some() {
+        println!("{}", out.unwrap());
+    }
+
+    Ok(target_dir.into_os_string().into_string().unwrap().to_string())
 }
 
+fn install_packages(python_install_dir: String, install_pypresence: bool, install_webview: bool, version: String) -> Result<(), String> {
+    let mut python_exe = PathBuf::new();
+    python_exe.push(python_install_dir);
+    python_exe.push("python.exe");
+
+    let rare_install;
+    if version == "stable" {
+        rare_install = "Rare"
+    } else {
+        rare_install = "git+https://github.com/Dummerle/Rare.git"
+    }
+
+    let python_exe_str = python_exe.into_os_string().into_string().unwrap();
+    let mut command = vec![python_exe_str.as_str(), "-m", "pip", "install", "-U", rare_install];
+
+    if install_pypresence {
+        command.push("pypresence");
+    }
+    if install_webview {
+        command.push("pywebview[cef]")
+    }
+
+    println!("{:?}", &command);
+
+    let mut process = match Popen::create(
+        &command,
+        PopenConfig {
+            stdout: Redirection::Pipe,
+            stderr: Redirection::Pipe,
+            ..Default::default()
+        },
+    ) {
+        Ok(proc) => proc,
+        Err(err) => return Err(err.to_string())
+    };
+
+    let (out, err) = match process.communicate(None) {
+        Ok(res) => res,
+        Err(err) => return Err(err.to_string())
+    };
+    if out.is_some() {
+        println!("{}", out.unwrap());
+    }
+    if err.is_some() {
+        println!("{}", err.unwrap());
+    }
+
+
+    Ok(())
+}*/
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
+extern crate core;
+
 mod install;
 
 use install::{AppState, Version};
 
 use druid::{AppDelegate, AppLauncher, Command, DelegateCtx, Env, EventCtx, Handled, LocalizedString, Target, UnitPoint, Widget, WidgetExt, WindowDesc};
 use druid::commands::QUIT_APP;
-use druid::widget::{Button, Checkbox, Flex, Label, RadioGroup};
+use druid::widget::{Button, Either, Flex, Label, RadioGroup};
 
 const WINDOW_TITLE: LocalizedString<AppState> = LocalizedString::new("Rare Updater");
 
@@ -35,7 +37,12 @@ impl AppDelegate<AppState> for Delegate {
             // If the command we received is `FINISH_SLOW_FUNCTION` handle the payload.
             data.set_info_text(text.to_string());
             Handled::Yes
-        } else {
+        } else if cmd.get(install::FINISHED).is_some() {
+            data.set_info_text("Finished".to_string());
+            Handled::Yes
+        }
+
+        else {
             Handled::No
         }
     }
@@ -48,34 +55,40 @@ fn build_root_widget() -> impl Widget<AppState> {
     // a label that will determine its text based on the current app data.
     let title = Label::new("Rare installer").with_text_size(30.0);
 
-    let pypresence_checkbox = Checkbox::new("PyPresence (To show running games on Discord)")
+   /* let pypresence_checkbox = Checkbox::new("PyPresence (To show running games on Discord)")
         .lens(AppState::install_pypresence);
 
     let webview_checkbox =
         Checkbox::new("PyWebview (For a easier login)").lens(AppState::install_webview);
-
+*/
     let radio = RadioGroup::new(versions.to_vec()).lens(AppState::version);
 
     let cancel_button = Button::new("Cancel").on_click(|ctx, _, _| {
         ctx.submit_command(QUIT_APP);
     });
+
     let install_button = Button::new("Install").
         on_click(|ctx: &mut EventCtx, data: &mut AppState, _env: &Env| {
-            let _ = install::install(ctx.get_external_handle(), data);
+            data.installing = true;
+            let _ = install::install(ctx.get_external_handle(), data.get_install_options());
         });
 
     let info_text = Label::new(|data: &AppState, _env: &Env| {
         data.info_text.to_string()
     });
 
-    let button_layout = Flex::row()
-        .with_child(cancel_button)
-        .with_child(install_button);
+    let button_layout = Either::new(
+        |data: &AppState, _env| !data.installing,
+        Flex::row()
+            .with_child(cancel_button)
+            .with_child(install_button),
+        Label::new("Installing"),
+    );
 
     let layout = Flex::column()
         .with_child(title)
-        .with_child(pypresence_checkbox)
-        .with_child(webview_checkbox)
+       // .with_child(pypresence_checkbox)
+       // .with_child(webview_checkbox)
         .with_child(radio)
         .with_child(info_text)
         .with_child(button_layout)
